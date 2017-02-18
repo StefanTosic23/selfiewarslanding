2734457c9c2480f7e2cfa9c4c3e625a224afa716
Dev <Dev>
2017-02-18 23:35:32 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include plugins
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var server = require('gulp-server-livereload');
+
+// Paths
+var paths = {
+
+	css: {
+		dest: 'css'
+	},
+
+	scss: {
+		src: 'sass/**/*',
+		main: 'sass/stylesheets/main.scss'
+	}
+
+};
+
+// Compile SCSS to CSS
+gulp.task('sass', function() {
+	gulp.src(paths.scss.main)
+	.pipe(sourcemaps.init())
+		.pipe(sass())
+		.on('error',console.log.bind(console))
+		.pipe(autoprefixer())
+	.pipe(sourcemaps.write('.'))
+	.pipe(gulp.dest(paths.css.dest));
+});
+
+
+gulp.task('webserver', function() {
+  gulp.src('')
+    .pipe(server({
+      livereload: true,
+      directoryListing: false,
+      open: true
+    }));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+	gulp.watch(paths.scss.src, ['sass']);
+});
+
+// Default Task
+gulp.task('default', ['watch']);
